ccc6ada31f5f14f7cb89cf1afcf3e6087198b1a0
Abdullah Iftikhar <abdullahiftikharabbasi8@gmail.com>
2024-08-04 16:12:15 +0500

    biznusEscape Template

---
+++ js/script.js
@@ -0,0 +1,22 @@
+const activepage = window.location.pathname;
+const navlinks = document.querySelectorAll(".nav-link");
+
+navlinks.forEach((link) => {
+	if (link.href && link.href.includes(activepage)) {
+		link.classList.add("active");
+	}
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+	const showNav = document.querySelector(".menu-button");
+	const mobileOverlay = document.querySelector(".mobile-overlay");
+	const hideNav = document.getElementById("close-nav");
+
+	showNav.addEventListener("click", function () {
+		mobileOverlay.style.transform = "translateX(0%)";
+	});
+
+	hideNav.addEventListener("click", function () {
+		mobileOverlay.style.transform = "translateX(100%)";
+	});
+});
