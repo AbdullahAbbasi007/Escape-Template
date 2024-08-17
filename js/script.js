30db59cd0b2e4fd6c47f5f45f41ca0275e1b89c8
Abdullah Iftikhar <abdullahiftikharabbasi8@gmail.com>
2024-08-17 11:08:38 +0500

    Add Script in some file

--- js/script.js
+++ js/script.js
@@ -11,12 +11,18 @@ document.addEventListener("DOMContentLoaded", function () {
 	const showNav = document.querySelector(".menu-button");
 	const mobileOverlay = document.querySelector(".mobile-overlay");
 	const hideNav = document.getElementById("close-nav");
+	const heading = document.querySelector(".hero-header");
+	const para = document.querySelector(".hero-para");
 
 	showNav.addEventListener("click", function () {
 		mobileOverlay.style.transform = "translateX(0%)";
+		heading.style.display = "none";
+		para.style.display = "none";
 	});
 
 	hideNav.addEventListener("click", function () {
 		mobileOverlay.style.transform = "translateX(100%)";
+		heading.style.display = "block";
+		para.style.display = "block";
 	});
 });
